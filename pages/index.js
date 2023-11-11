60775d73105c75067f8a6cb810553fc36ee3e541
maheshwariparin <parinkm007@gmail.com>
2023-11-11 14:02:02 +0530

    login/signup

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+
+import Header1 from "/components/Header1";
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>
+         property dealing app
+        </title>
+      </Head>
+      <Header1 />
+    </div>
+  );
+};
+
+export default Home;
